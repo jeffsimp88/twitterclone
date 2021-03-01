4c4860e404938ac5ca73415dd381cdd3457a9cbc
jeffsimp88 <jdsimpson806@gmail.com>
2021-03-01 14:59:01 -0500

    Updated index_view to show recent tweets

--- twitteruser/views.py
+++ twitteruser/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render, redirect, HttpResponseRedirect
 from twitteruser.models import CustomUser
+from tweet.models import Tweet
 from django.contrib.auth.decorators import login_required
 
 
 @login_required
 def index_view(request):
-    users_list = CustomUser.objects.all()
-    context = {'heading': f'Welcome, {request.user.username}!', 'users_list': users_list}
+    tweet_list = Tweet.objects.all()[::-1]
+    context = {'heading': f'Welcome, {request.user.username}!', 'tweet_list': tweet_list}
     return render(request, 'index.html', context)
