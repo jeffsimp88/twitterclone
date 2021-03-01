e556338d81e35be30e4b341c23f9e550f3e6dc62
jeffsimp88 <jdsimpson806@gmail.com>
2021-03-01 12:39:21 -0500

    Added static, template, and apps

--- twitterclone/settings.py
+++ twitterclone/settings.py
@@ -25,7 +25,13 @@ SECRET_KEY = '_suh41u8b7!d7e87@7acbntmw%hb)ug8f*hxu)sf164=+wr4+u'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*',]
+
+LOGIN_URL = "/login/"
+
+STATICFILES_DIRS = [
+    BASE_DIR/'static'
+]
 
 
 # Application definition
@@ -37,6 +43,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'twitteruser'
 ]
 
 MIDDLEWARE = [
@@ -54,7 +61,7 @@ ROOT_URLCONF = 'twitterclone.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR/'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -118,3 +125,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# AUTH_USER_MODEL = 'ticket_app.CustomUser'
