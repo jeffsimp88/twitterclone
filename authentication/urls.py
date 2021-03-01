56902e5c2b8884242b195e6e49844908192fa194
jeffsimp88 <jdsimpson806@gmail.com>
2021-03-01 14:00:32 -0500

    Added path to logout

--- authentication/urls.py
+++ authentication/urls.py
@@ -5,4 +5,5 @@ from authentication import views
 urlpatterns = [
     path('login/', views.login_view, name='login'),
     path('signup/', views.signup_view, name='signup'),
+    path('logout/', views.logout_view, name='logout')
 ]
