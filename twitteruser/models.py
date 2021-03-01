b4cd889f7161aa1c844a30b152c253e305f67785
jeffsimp88 <jdsimpson806@gmail.com>
2021-03-01 12:49:08 -0500

    Created custom user model

--- twitteruser/models.py
+++ twitteruser/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+class CustomUser(AbstractUser):
+    display_name = models.CharField( max_length=50, null=True, blank=True)
+    email = models.EmailField( max_length=254, null=True, blank=True)
+    
+    USERNAME_FIELD='username'
+    REQUIRED_FIELDS = []
+
+    def __str__(self):
+        return self.username
