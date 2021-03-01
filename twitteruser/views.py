3fe1b8a0489b4ec0f93f09074c2adb4d750f9cc4
jeffsimp88 <jdsimpson806@gmail.com>
2021-03-01 14:01:47 -0500

    Updated index_view to show list of all users

--- twitteruser/views.py
+++ twitteruser/views.py
@@ -5,5 +5,6 @@ from django.contrib.auth.decorators import login_required
 
 @login_required
 def index_view(request):
-    context = {'heading': f'Welcome, {request.user.username}!'}
+    users_list = CustomUser.objects.all()
+    context = {'heading': f'Welcome, {request.user.username}!', 'users_list': users_list}
     return render(request, 'index.html', context)
