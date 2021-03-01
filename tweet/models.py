1968797d314c2ef9bceae029d1cd9c5f5b85bdd6
jeffsimp88 <jdsimpson806@gmail.com>
2021-03-01 14:33:12 -0500

    Created tweet model

--- tweet/models.py
+++ tweet/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.conf import settings
+from django.utils import timezone
 
-# Create your models here.
+class Tweet(models.Model):
+    message = models.TextField(max_length=140)
+    date_posted = models.DateTimeField(default=timezone.now)
+    post_user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name = 'post_user'
+        )
+    def __str__(self):
+        return f'{self.message} | {self.post_user}'
