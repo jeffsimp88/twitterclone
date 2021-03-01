030f9776e8ff74f157787a2b4e633f8efe6dee7e
jeffsimp88 <jdsimpson806@gmail.com>
2021-03-01 13:41:16 -0500

    removed login & signup form

--- twitteruser/forms.py
+++ twitteruser/forms.py
@@ -12,13 +12,3 @@ class CustomUserChangeForm(UserChangeForm):
     class Meta(UserChangeForm):
         model = CustomUser
         fields = ('username', 'display_name', 'email',)
-
-class SignupForm(forms.Form):
-    username = forms.CharField(max_length=100)
-    display_name= forms.CharField(max_length=50)
-    email = forms.EmailField(max_length=250)
-    password = forms.CharField(widget=forms.PasswordInput)
-
-class LoginForm(forms.Form):
-    username = forms.CharField(max_length=40)
-    password = forms.CharField(widget=forms.PasswordInput)
