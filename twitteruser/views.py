f83dc82c1d65718766292ec8f5b7f3d489053812
jeffsimp88 <jdsimpson806@gmail.com>
2021-03-01 15:15:25 -0500

    Created a profile_view

--- twitteruser/views.py
+++ twitteruser/views.py
@@ -9,3 +9,13 @@ def index_view(request):
     tweet_list = Tweet.objects.all()[::-1]
     context = {'heading': f'Welcome, {request.user.username}!', 'tweet_list': tweet_list}
     return render(request, 'index.html', context)
+
+def profile_view(request, user_name):
+    user_info = CustomUser.objects.get(username=user_name)
+    tweets = Tweet.objects.filter(post_user=user_info)
+    context = {
+        'heading': f'The Profile Page of {user_info.display_name}',
+        'user': user_info,
+        'tweets': tweets
+        }
+    return render(request, 'user_page.html', context)
