47cf607e9675514c1a4e61aa90b37bf8b9a06ee5
jeffsimp88 <jdsimpson806@gmail.com>
2021-03-02 13:29:55 -0500

    Added a view to see a specific tweet page

--- tweet/views.py
+++ tweet/views.py
@@ -17,3 +17,10 @@ def post_tweet_view(request):
     form = TweetForm()
     context = {'heading': 'Post a Tweet', 'form': form}
     return render(request, 'forms.html', context)
+
+def tweet_view(request, tweet_id):
+    context = {'heading': "Tweet Details"}
+    tweet = Tweet.objects.get(id=tweet_id)
+    context.update({'tweet': tweet})
+    return render(request, 'tweet_details.html', context)
+    
