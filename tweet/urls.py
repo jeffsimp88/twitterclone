dda9a7e9450417604e0e3a535e5f3790b6ff4d70
jeffsimp88 <jdsimpson806@gmail.com>
2021-03-03 15:13:20 -0500

    Added url path for delete tweet

--- tweet/urls.py
+++ tweet/urls.py
@@ -4,5 +4,6 @@ from tweet import views
 
 urlpatterns = [
     path('posttweet/', views.post_tweet_view, name='post tweet'),
-    path('tweet/<int:tweet_id>/', views.tweet_view, name="tweet details")
+    path('tweet/<int:tweet_id>/', views.tweet_view, name="tweet details"),
+    path('deletetweet/<int:tweet_id>/', views.delete_tweet_view, name="delete tweet")
 ]
