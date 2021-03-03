9b617254b31bc42d8efc12965bb5cf4940a61c79
jeffsimp88 <jdsimpson806@gmail.com>
2021-03-03 14:24:50 -0500

    Added a path to old notifications

--- notifications/urls.py
+++ notifications/urls.py
@@ -4,4 +4,5 @@ from notifications import views
 
 urlpatterns = [
     path("notifications/", views.notifications_view, name="notifications"),
+    path("notifications/old", views.old_notification_view, name="old notifications"),
 ]
