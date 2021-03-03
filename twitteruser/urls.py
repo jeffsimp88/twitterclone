7ccc27892b922790595c882f5b1ac92b878a0972
jeffsimp88 <jdsimpson806@gmail.com>
2021-03-03 14:33:41 -0500

    Added path to recent tweets view

--- twitteruser/urls.py
+++ twitteruser/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('', views.index_view, name='homepage'),
     path('users/<user_name>/', views.profile_view, name='profile page'),
     path('follow/<str:user_name>', views.follow_user, name='follow'),
+    path('recenttweets/', views.recent_tweets_view, name='recent tweets')
 ]
