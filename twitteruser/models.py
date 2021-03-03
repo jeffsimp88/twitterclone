cf87c84cf7dbbe482fac1be591c540101d61ec89
jeffsimp88 <jdsimpson806@gmail.com>
2021-03-03 09:07:11 -0500

    Removed Followers model, merged with CustomUser

--- twitteruser/models.py
+++ twitteruser/models.py
@@ -6,16 +6,10 @@ from django.contrib.auth.models import AbstractUser
 class CustomUser(AbstractUser):
     display_name = models.CharField( max_length=50, null=True, blank=True)
     email = models.EmailField( max_length=254, null=True, blank=True)
+    following_user = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name='users_following', blank=True)
     
     USERNAME_FIELD='username'
     REQUIRED_FIELDS = []
 
     def __str__(self):
         return self.username
-
-class Followers(models.Model):
-    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-    following_user = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name='following_user', null=True, blank=True)
-
-    def __str__(self):
-        return self.user.username
