0241ffba0295105597f59f8e49888d63f8f8966c
jeffsimp88 <jdsimpson806@gmail.com>
2021-03-03 13:25:14 -0500

    Imported notifications functions

--- tweet/views.py
+++ tweet/views.py
@@ -2,6 +2,10 @@ from django.shortcuts import render, redirect, HttpResponseRedirect
 from django.contrib.auth.decorators import login_required
 from tweet.models import Tweet
 from tweet.forms import TweetForm
+from twitteruser.models import CustomUser
+from notifications.models import Notification
+from notifications.views import check_notifications, create_notification
+
 
 @login_required
 def post_tweet_view(request):
@@ -13,13 +17,16 @@ def post_tweet_view(request):
                 message=data['message'],
                 post_user = request.user
             )
+            create_notification(new_tweet)
             return HttpResponseRedirect('/')
     form = TweetForm()
-    context = {'heading': 'Post a Tweet', 'form': form}
+    notifications = check_notifications(request.user)
+    context = {'heading': 'Post a Tweet', 'form': form, 'notifications': notifications}
     return render(request, 'forms.html', context)
 
 def tweet_view(request, tweet_id):
-    context = {'heading': "Tweet Details"}
+    notifications = check_notifications(request.user)
+    context = {'heading': "Tweet Details", 'notifications': notifications}
     tweet = Tweet.objects.get(id=tweet_id)
     context.update({'tweet': tweet})
     return render(request, 'tweet_details.html', context)
