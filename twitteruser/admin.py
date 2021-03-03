51d7e153b4b888a668b3e2c5e4c60ad19d1b19cf
jeffsimp88 <jdsimpson806@gmail.com>
2021-03-03 09:07:41 -0500

    Removed Followers, updated CustomUserAdmin

--- twitteruser/admin.py
+++ twitteruser/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
-from twitteruser.models import CustomUser, Followers
+from twitteruser.models import CustomUser
 from twitteruser.forms import CustomUserChangeForm, CustomUserForm
 
 class CustomUserAdmin(UserAdmin):
@@ -10,17 +10,17 @@ class CustomUserAdmin(UserAdmin):
     list_display = ('username', 'display_name', 'email', 'is_staff', 'is_active',)
     list_filter = ('username', 'display_name', 'email', 'is_staff', 'is_active',)
     fieldsets = (
-        (None, {"fields": ('username', 'display_name', 'email', 'password')}),
+        (None, {"fields": ('username', 'display_name', 'email', 'following_user', 'password')}),
             ('Permissions', {'fields': ('is_staff', 'is_active')})
     )
     add_fieldsets  = (
         (None, {
             "classes": ('wide',),
-            "fields": ('username', 'display_name', 'email', 'password1', 'password2', 'is_staff', 'is_active')
+            "fields": ('username', 'display_name', 'email', 'following_user', 'password1', 'password2', 'is_staff', 'is_active')
         }),
     )
     search_fields = ('display_name', 'email')
     ordering = ('username', 'display_name', 'email',)
 
 admin.site.register(CustomUser, CustomUserAdmin)
-admin.site.register(Followers)
+# admin.site.register(Followers)
