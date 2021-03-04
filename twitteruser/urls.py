1609100d16648b5b4e6b63fe84e201b08bb74540
jeffsimp88 <jdsimpson806@gmail.com>
2021-03-04 15:00:47 -0500

    Added '/' to url paths

--- twitteruser/urls.py
+++ twitteruser/urls.py
@@ -5,6 +5,6 @@ from twitteruser import views
 urlpatterns = [
     path('', views.index_view, name='homepage'),
     path('users/<user_name>/', views.profile_view, name='profile page'),
-    path('follow/<str:user_name>', views.follow_user, name='follow'),
+    path('follow/<str:user_name>/', views.follow_user, name='follow'),
     path('recenttweets/', views.recent_tweets_view, name='recent tweets')
 ]
